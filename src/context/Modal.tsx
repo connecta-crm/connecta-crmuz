ec05b9d696857c0a35dcd7606809dd9a5651fc1b
tulkin07 <akhmadov0770@gmail.com>
2024-04-26 23:18:49 +0500

    fexed context

--- src/context/Modal.tsx
+++ src/context/Modal.tsx
@@ -1,12 +1,17 @@
 import React, { createContext, useContext, useState } from "react";
 
-const ModalContext = createContext();
+type ModalContextType = {
+    show: boolean;
+    hideModal: (a: boolean) => void;
+}
+
+const ModalContext = createContext<ModalContextType | null>(null);
 
 
-const ModalProvider = ({ children }:{children:React.ReactNode}) => {
+const ModalProvider = ({ children }: { children: React.ReactNode }) => {
     const [show, hideModal] = useState(false)
     return (
-        <ModalContext.Provider value={{ show,hideModal }}>
+        <ModalContext.Provider value={{ show, hideModal }}>
             {children}
         </ModalContext.Provider>
     )
