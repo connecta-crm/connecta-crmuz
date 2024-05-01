c893d03667fb4d49bd48166298b7a1d7a765c185
j.bektemirov <b.o.jakhongir@gmail.com>
2024-05-02 02:01:42 +0500

    change helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,23 +1,23 @@
-import { useCallback, useRef } from 'react';
+// import { useCallback, useRef } from 'react';
 
-export function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}
+// export function classNames(...classes: string[]) {
+//   return classes.filter(Boolean).join(' ');
+// }
 
-export function useDebounce(callback: () => void, delay: number) {
-  const timer = useRef(null);
+// export function useDebounce(callback: () => void, delay: number) {
+//   const timer = useRef(null);
 
-  const debouncedCallback = useCallback(
-    (...args: unknown[]) => {
-      if (timer.current) {
-        clearTimeout(timer.current);
-      }
-      timer.current = setTimeout(() => {
-        callback(...args);
-      }, delay);
-    },
-    [callback, delay],
-  );
+//   const debouncedCallback = useCallback(
+//     (...args: unknown[]) => {
+//       if (timer.current) {
+//         clearTimeout(timer.current);
+//       }
+//       timer.current = setTimeout(() => {
+//         callback(...args);
+//       }, delay);
+//     },
+//     [callback, delay],
+//   );
 
-  return debouncedCallback;
-}
+//   return debouncedCallback;
+// }
