c373480918dcc22316a937a061e973ae130c41ba
j.bektemirov <b.o.jakhongir@gmail.com>
2024-05-17 00:06:05 +0500

    implement global modal UI

---
+++ src/ui/Modal.tsx
@@ -0,0 +1,72 @@
+import { LoadingOutlined } from '@ant-design/icons';
+import { Button, Modal as ModalUI } from 'antd';
+import { ReactNode } from 'react';
+
+type ModalProps = {
+  title: string;
+  loading: boolean;
+  open: boolean;
+  width: 'small' | 'middle' | 'large';
+  padding: '0' | '15';
+  children: ReactNode;
+  onSave: () => void;
+  onCancel: () => void;
+};
+
+function Modal({
+  title,
+  open = false,
+  loading,
+  padding = '15',
+  width = 'small',
+  onSave,
+  onCancel,
+  children,
+}: ModalProps) {
+  const modalWidth = width === 'small' ? 420 : width === 'middle' ? 840 : 915;
+
+  return (
+    <>
+      <ModalUI
+        className="modal"
+        title={
+          <div className="modal__header">
+            <h2>{title}</h2>
+            <div>
+              <Button size="small" onClick={onCancel}>
+                Cancel
+              </Button>
+              <Button
+                size="small"
+                type="primary"
+                className="ml-10"
+                disabled={loading}
+                onClick={onSave}
+              >
+                {!loading ? (
+                  'Save'
+                ) : (
+                  <>
+                    Save <LoadingOutlined />
+                  </>
+                )}
+              </Button>
+            </div>
+          </div>
+        }
+        open={open}
+        width={modalWidth}
+        footer={null}
+        closable={false}
+        maskClosable={false}
+        afterClose={() => {}}
+      >
+        <div className="modal__body" style={{ padding: Number(padding) }}>
+          {children}
+        </div>
+      </ModalUI>
+    </>
+  );
+}
+
+export default Modal;
